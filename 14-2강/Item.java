5c2ddf462c221bff801a4a8b5abc630a09f7be27
dogyunahn <67811528+dogyunahn@users.noreply.github.com>
2021-03-29 00:24:34 +0900

    14.JPA 연관관계 설정 배우기 - 코딩하기 - 2

---
+++ 14-2강/Item.java
@@ -0,0 +1,38 @@
+package com.example.study.model.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Item {
+
+    @Id
+@@ -26,11 +25,8 @@
+
+    private String content;
+
+    private String title;
+
+    private Long partnerId;
+
+
+
+    //1:N
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
+    private List<OrderDetail> orderDetailList;
+
+}
