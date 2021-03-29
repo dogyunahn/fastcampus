dba2571d9c6184ea457c414e1b5976a7ded8e2f3
dogyunahn <67811528+dogyunahn@users.noreply.github.com>
2021-03-29 12:20:37 +0900

    15.JPA 연관관계 설정 배우기 - QueryMethod

--- 15강/Item.java
+++ 15강/Item.java
@@ -1,15 +1,29 @@
+package com.example.study.model.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@@ -19,14 +20,35 @@
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Item{
     private Long id;
 
- 
+   
+    private String content;
+
+    private String title;
+
+    private Long partnerId;
+
     private String name;
 
     private Integer price;
