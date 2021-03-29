557fed01818a294cc8af50f5d7a40b5f46fef7f2
dogyunahn <67811528+dogyunahn@users.noreply.github.com>
2021-03-29 11:38:01 +0900

    15.JPA 연관관계 설정 배우기 - QueryMethod

---
+++ 15강/User.java
@@ -0,0 +1,50 @@
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor     //모든 매개변수를 가지는 생성자 생성.
+@NoArgsConstructor      //기본 생성자.
+@Entity     //==table
+//@Table(name = "User")   //클래스와 테이블의 이름이 같다면 생략가능
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  
+
+  //@Column(name = "account") 이것도 실제 테이블의 컬럼명과 변수명이 동일 하기 때문에 생략 가능.
+    private String account;
+
+    private String password;
+
+    private String status;
+
+    private String email;
+
+    //테이블에서는 phone_number이지만 JPA에서 자동으로 매칭 시켜줌.
+    private String phoneNumber;
+
+    private LocalDateTime registerdAt;
+
+    private LocalDateTime unregisterdAt;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+
+
+
+    //.LAZY = 지연 로딩 - 해당 테이블 하나만 FROM 해서 SELECT - 시간 적게 걸림.
+    //.EAGER = 즉시 로딩 - 연관된 테이블 전부 LEFT OUTER JOIN 해서 SELECT - 시간 많이 걸림.(보통 테이블 하나만 걸릴때 사용)
+    //User 테이블 입장에서는 1명의 유저, N개의 구매목록이므로 @OneToMany
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")   //mappedBy는 orderDetail에 있는 변수명(user)과 동일 해야함.
+                                                            //
+
+    private List<OrderDetail> orderDetailList;      //OrderDetail의 변수 user와 매칭 시킨다.
+                                                    //이렇게 user 클래스와 OrderDetail에서 1:N관계가 만들어진것.
+}
